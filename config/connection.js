1b79a07a8708908d802e24bbd4456ca0652a192a
Hunter Matthews <sirhunty@gmail.com>
2020-05-04 13:01:34 -0700

    removed node modules; added gitignore file, and updated mysql connection with pwassword

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "root",
     database: "burgers_db"
   });
 }
