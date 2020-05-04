190f954151053c18af7228f5152e0738cc7f7b2c
Hunter Matthews <sirhunty@gmail.com>
2020-05-04 16:36:38 -0700

    updated orm and connection files

--- config/orm.js
+++ config/orm.js
@@ -1,85 +1,76 @@
-const connection = require("../config/connection");
+// Here is the O.R.M. where you write functions that takes inputs and conditions
+// and turns them into database commands like SQL.
 
-function createQmarks(num) {
-  let arr = [];
-  for (let i = 0; i < num; i++) {
-    arr.push("?");
-  }
-  return arr.toString();
-}
+var connection = require("./connection.js");
 
-function translateSql(ob) {
-  let arr = [];
-  for (let key in ob) {
-    let value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
     }
-  }
-  return arr.toString();
+    return arr.toString();
 }
 
-let orm = {
-  selectAll: function(table, cb) {
-    let dbQuery = "SELECT * FROM " + table + ";";
+function objToSql(ob) {
+    // column1=value, column2=value2,...
+    var arr = [];
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  insertOne: function(table, cols, vals, cb) {
-    let dbQuery =
-      "INSERT INTO " +
-      table +
-      " (" +
-      cols.toString() +
-      ") " +
-      "VALUES (" +
-      createQmarks(vals.length) +
-      ") ";
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+    return arr.toString();
+}
 
-    console.log(dbQuery);
-    connection.query(dbQuery, vals, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  updateOne: function(table, objColVals, condition, cb) {
-    let dbQuery =
-      "UPDATE " +
-      table +
-      " SET " +
-      translateSql(objColVals) +
-      " WHERE " +
-      condition;
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // vals is an array of values that we want to save to cols
+    // cols are the columns we want to insert the values into
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
 
-    console.log(dbQuery);
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  },
-  deleteOne: function(table, condition, cb) {
-    let dbQuery = "DELETE FROM " + table + " WHERE " + condition;
-    console.log(dbQuery);
+        console.log(queryString);
 
-    connection.query(dbQuery, function(err, res) {
-      if (err) {
-        throw err;
-      }
-      cb(res);
-    });
-  }
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // objColVals would be the columns and values that you want to update
+    // an example of objColVals would be {name: panther, sleepy: true}
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 };
+
 module.exports = orm;
